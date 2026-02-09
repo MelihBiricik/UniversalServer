330c313a5a1e995c146f9ee9afd9131af1c6c6eb
MelihErenBiricik <MelihErenBiricik@ENGAAD043918>
2026-02-09 08:19:25 +0100

    Datenbankzugriff und Verbindung konfiguriert

--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -18,11 +18,10 @@ namespace UniversalServer.Model
             // 
             _myConnection =
                      new MySqlConnection(
-                         "SERVER=localhost;" +
-                         "DATABASE=smarthome;" +
+                         "SERVER=SmartHome;" +
+                         "DATABASE=SmartHomeDB;" +
                          "UID = root;" +
-                         "PASSWORD=;");
-
+                         "PASSWORD=Baklava;");
         }
 
         public void OpenConnectionToDBServer()
@@ -40,9 +39,49 @@ namespace UniversalServer.Model
 
         public void InsertData(TempValue tv, HumidValue hv, PressureValue pv, DateTime dt, string ip)
         {
-            string msg = $"Das ist die Temperatur: {tv.Value}\n Das ist die Feuchtigkeit: {hv.Value}\n Das ist der Luftdruck: {pv.Value}\n";
-            MessageBox.Show(msg);
-      
+            // string msg = $"Das ist die Temperatur: {tv.Value}\n Das ist die Feuchtigkeit: {hv.Value}\n Das ist der Luftdruck: {pv.Value}\n";
+            // MessageBox.Show(msg);
+
+            try
+            {
+                OpenConnectionToDBServer(); // Verbindung zur MySQL Workbench öffnen
+
+                // SCHRITT 1: Die SensorID für den Namen (z.B. "Wohnzimmer") herausfinden
+                string getSensorIdQuery = "SELECT SensorID FROM Sensor WHERE Typ = @name LIMIT 1";
+                MySqlCommand getLog = new MySqlCommand(getSensorIdQuery, _myConnection);
+                //getLog.Parameters.AddWithValue("@name", sensorName);
+
+                // Wir führen den Befehl aus und holen uns die ID
+                object result = getLog.ExecuteScalar();
+
+                if (result != null)
+                {
+                    int sID = Convert.ToInt32(result);
+
+                    // SCHRITT 2: Den Messwert mit der gefundenen SensorID speichern
+                    string insertQuery = "INSERT INTO Messwerte (SensorID, Temperatur, Luftfeuchtigkeit, Luftdruck, Zeitpunkt) " +
+                                         "VALUES (@sID, @temp, @hum, @press, @time)";
+
+                    MySqlCommand cmd = new MySqlCommand(insertQuery, _myConnection);
+                    cmd.Parameters.AddWithValue("@sID", sID);
+                    cmd.Parameters.AddWithValue("@temp", tv.Value);
+                    cmd.Parameters.AddWithValue("@hum", hv.Value);
+                    cmd.Parameters.AddWithValue("@press", pv.Value);
+                    cmd.Parameters.AddWithValue("@time", dt);
+
+                    cmd.ExecuteNonQuery(); // Daten in die Datenbank schreiben
+                }
+                else
+                {
+                    //MessageBox.Show("Sensor '" + sensorName + "' wurde nicht in der Datenbank gefunden!");
+                }
+
+                _myConnection.Close(); // Tür wieder schließen
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Speichern: " + ex.Message);
+            }
 
         }
     }
