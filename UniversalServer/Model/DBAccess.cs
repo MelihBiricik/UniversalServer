cf5e5fc4f29c5067ce24744c93aa763e6bf3dcd6
Melih.Biricik <Melih.Biricik@R1506-NB11>
2026-03-19 14:15:14 +0100

    Bearbeitung des SQL Verbindung

--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -1,8 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,18 +14,16 @@ namespace UniversalServer.Model
     public class DBAccess
     {
         private MySqlConnection _myConnection;
-
         public DBAccess()
         {
             // 
             _myConnection =
                      new MySqlConnection(
-                         "SERVER=SmartHome;" +
-                         "DATABASE=SmartHomeDB;" +
+                         "SERVER=localhost;" +
+                         "DATABASE=SmartHomeDB2;" +
                          "UID = root;" +
-                         "PASSWORD=Baklava;");
+                         "PASSWORD=;");
         }
-
         public void OpenConnectionToDBServer()
         {
             try
@@ -36,54 +36,56 @@ namespace UniversalServer.Model
                 throw ex2;
             }
         }
-
         public void InsertData(TempValue tv, HumidValue hv, PressureValue pv, DateTime dt, string ip)
         {
-            // string msg = $"Das ist die Temperatur: {tv.Value}\n Das ist die Feuchtigkeit: {hv.Value}\n Das ist der Luftdruck: {pv.Value}\n";
-            // MessageBox.Show(msg);
-
             try
             {
                 OpenConnectionToDBServer(); // Verbindung zur MySQL Workbench öffnen
 
                 // SCHRITT 1: Die SensorID für den Namen (z.B. "Wohnzimmer") herausfinden
                 string getSensorIdQuery = "SELECT SensorID FROM Sensor WHERE Typ = @name LIMIT 1";
+                int sID = -1;
+
                 MySqlCommand getLog = new MySqlCommand(getSensorIdQuery, _myConnection);
-                //getLog.Parameters.AddWithValue("@name", sensorName);
+
+                getLog.Parameters.AddWithValue("@name", ip);
 
                 // Wir führen den Befehl aus und holen uns die ID
                 object result = getLog.ExecuteScalar();
 
                 if (result != null)
                 {
-                    int sID = Convert.ToInt32(result);
+                    sID = Convert.ToInt32(result);
+                }
+                if (sID != -1) 
+                {  
+                // SCHRITT 2: Den Messwert mit der gefundenen SensorID speichern
+                    string insertQuery = "INSERT INTO Messwerte (Zeitpunkt, Temperatur, Luftfeuchtigkeit, Luftdruck, SensorID)" + 
+                                         "VALUES(@time, @temp, @hum, @press, @sID)";
 
-                    // SCHRITT 2: Den Messwert mit der gefundenen SensorID speichern
-                    string insertQuery = "INSERT INTO Messwerte (SensorID, Temperatur, Luftfeuchtigkeit, Luftdruck, Zeitpunkt) " +
-                                         "VALUES (@sID, @temp, @hum, @press, @time)";
 
-                    MySqlCommand cmd = new MySqlCommand(insertQuery, _myConnection);
-                    cmd.Parameters.AddWithValue("@sID", sID);
-                    cmd.Parameters.AddWithValue("@temp", tv.Value);
-                    cmd.Parameters.AddWithValue("@hum", hv.Value);
-                    cmd.Parameters.AddWithValue("@press", pv.Value);
-                    cmd.Parameters.AddWithValue("@time", dt);
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, _myConnection))
+                    {
+                        // Parametreleri şemadaki DECIMAL(5,2) yapısına uygun gönderiyoruz
+                        cmd.Parameters.AddWithValue("@time", dt);
+                        cmd.Parameters.AddWithValue("@temp", tv.Value);
+                        cmd.Parameters.AddWithValue("@hum", hv.Value);
+                        cmd.Parameters.AddWithValue("@press", pv.Value);
+                        cmd.Parameters.AddWithValue("@sID", sID);
 
-                    cmd.ExecuteNonQuery(); // Daten in die Datenbank schreiben
+                        cmd.ExecuteNonQuery(); // Daten in die Datenbank schreiben
+                    }
                 }
                 else
                 {
-                    //MessageBox.Show("Sensor '" + sensorName + "' wurde nicht in der Datenbank gefunden!");
+                    MessageBox.Show("Sensor '" + ip + "' wurde nicht in der Datenbank gefunden!");
                 }
-
                 _myConnection.Close(); // Tür wieder schließen
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Fehler beim Speichern: " + ex.Message);
             }
-
         }
     }
-
 }
